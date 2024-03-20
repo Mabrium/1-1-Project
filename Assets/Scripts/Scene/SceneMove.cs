51477acc43491bee14071136020751bfc5be767a
Mabrium <gangseonghun24@gmail.com>
2024-03-20 15:10:13 +0900

    Speeddddd!!

--- Assets/Scripts/Scene/SceneMove.cs
+++ Assets/Scripts/Scene/SceneMove.cs
@@ -5,17 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneMove : MonoBehaviour
 {
-    public Setting Setting;
+    public GameObject Setting;
     private int Xvalue = 0;
+    private int Yvalue = -1100;
+    private bool UpDown = false;
     public int MoveX = 0;
-    public int MoveXvlaue = 0;
+    public int MoveXvalue = 0;
+    public int MoveYvalue1 = 0;
+    public int MoveYvalue2 = -1100;
     public bool Moving = false;
     public GameObject Move;
     private RectTransform rectTransform;
     // Start is called before the first frame update
     void Start()
     {
-        Setting = GetComponent<Setting>();
+        
         rectTransform = GetComponent<RectTransform>();
     }
 
@@ -32,7 +36,14 @@ public class SceneMove : MonoBehaviour
                     case 0:
                         SceneManager.LoadScene("Start"); break;
                     case 1:
+                        if (!UpDown)
+                        {
+                            StartCoroutine(MoveY_Up());
+                        }
+                        else StartCoroutine(MoveY_Down());
                         break;
+                    case 2:
+                        Application.Quit(); break;
 
                 }
             }
@@ -41,9 +52,9 @@ public class SceneMove : MonoBehaviour
                 if (MoveX < 2)
                 {
                     MoveX++;
-                    if (MoveX == 0) MoveXvlaue = 0;
-                    if (MoveX == 1) MoveXvlaue = -1500;
-                    if (MoveX == 2) MoveXvlaue = -3000;
+                    if (MoveX == 0) MoveXvalue = 0;
+                    if (MoveX == 1) MoveXvalue = -1500;
+                    if (MoveX == 2) MoveXvalue = -3000;
                     StartCoroutine(MoveScrollRight());
                 }
             }
@@ -52,9 +63,9 @@ public class SceneMove : MonoBehaviour
                 if(MoveX > 0) 
                 {
                     MoveX--;
-                    if (MoveX == 0) MoveXvlaue = 0;
-                    if (MoveX == 1) MoveXvlaue = -1500;
-                    if (MoveX == 2) MoveXvlaue = -3000;
+                    if (MoveX == 0) MoveXvalue = 0;
+                    if (MoveX == 1) MoveXvalue = -1500;
+                    if (MoveX == 2) MoveXvalue = -3000;
                     StartCoroutine(MoveScrollLeft());
                 }
             }
@@ -65,11 +76,11 @@ public class SceneMove : MonoBehaviour
     {
             Moving = true;
             
-            while (Xvalue >= MoveXvlaue)
+            while (Xvalue >= MoveXvalue)
             {
-                rectTransform.anchoredPosition = new Vector3(Xvalue, 0, 0);
+                rectTransform.anchoredPosition = new Vector2(Xvalue, 0);
                 yield return null;
-                Xvalue -= 5;
+                Xvalue -= 10;
             }
             Moving = false;
     }
@@ -77,12 +88,32 @@ public class SceneMove : MonoBehaviour
     {
         Moving = true;
 
-        while (Xvalue <= MoveXvlaue)
+        while (Xvalue <= MoveXvalue)
         {
-            rectTransform.anchoredPosition = new Vector3(Xvalue, 0, 0);
+            rectTransform.anchoredPosition = new Vector2(Xvalue, 0);
             yield return null;
-            Xvalue += 5;
+            Xvalue += 10;
         }
         Moving = false;
+    }
+    IEnumerator MoveY_Up()
+    {
+        while (Yvalue <= MoveYvalue1)
+        {
+            Setting.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Yvalue);
+            yield return null;
+            Yvalue += 10;
+        }
+        UpDown = true;
+    }
+    IEnumerator MoveY_Down()
+    {
+        while (Yvalue >= MoveYvalue2)
+        {
+            Setting.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Yvalue);
+            yield return null;
+            Yvalue -= 10;
+        }
+        UpDown = false;
     }
 }
