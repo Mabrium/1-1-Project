0981f73a1672136e7615f873a81548eab02dbdd1
Mabrium7 <gangseonghun24@gmail.com>
2024-04-01 02:11:33 +0900

    Oh

---
+++ Assets/Scripts/Manager/GameObjectManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class GameObjectManager : MonoBehaviour
+{
+    //void Awake()
+    //{
+    //    //ver objs = FindObjectsType<>
+    //    DontDestroyOnLoad(gameObject);
+    //    if (objs.Length == 1)
+    //    {
+    //        Destroy(gameObject);
+    //        return;
+    //    }
+    //    else
+    //    {
+            
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Null/Press.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Press : MonoBehaviour
+{
+    public FadeInOut Fade;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartCoroutine(Starting());
+        }
+    }
+
+
+    IEnumerator Starting()
+    {
+        Fade.Fade();
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene("Menu");
+    }
+
+    
+}
