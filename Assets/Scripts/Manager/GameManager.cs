6e7ab7eb4537d8bb8d90daffe4dd7b7a70c15198
Mabrium <gangseonghun24@gmail.com>
2024-04-15 20:37:37 +0900

    f**k

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    private static GameManager Instance = null;
     public int SaveStage; //저장할 클리어된 스테이지 값
     public float SaveSoundVoulme; //저장된 사운드 값
 
+    private void Awake()
+    {
+        Instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+            //DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
         LoadData();
