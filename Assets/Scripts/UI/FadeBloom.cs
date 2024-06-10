cb8fe4fe95fedadb1d4cd2a2ee7b8e4d3a98320d
Mabrium <gangseonghun24@gmail.com>
2024-06-10 20:50:59 +0900

    Bloombbbb

---
+++ Assets/Scripts/UI/FadeBloom.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class FadeBloom : MonoBehaviour
+{
+    public Bloom bloom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void BloomFade()
+    {
+
+    }
+}
